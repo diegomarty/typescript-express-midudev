17bd7a4cd7e96460fc6beed23c777e68483c6f2a
Diego <dms509@hotmail.com>
2023-01-03 23:29:21 +0100

    ✨ Added types and interfaces

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+
+export interface DiaryEntry {
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
+}
+
+// export type NonSensitiveDiaryEntry = Pick<DiaryEntry, 'id'| 'date' | 'weather' | 'visibility'>
+
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>
