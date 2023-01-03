879ac432437dfb2a0999b788d294525e870f2eda
Diego <dms509@hotmail.com>
2023-01-03 23:29:02 +0100

    ✨ Added new routes and format

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+import diaryRouter from './routes/diaries'
 
-app.use(express.json()); // for parsing application/json
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json()) // for parsing application/json
+
+const PORT = process.env.PORT || 3000
 
 // _ means that the variable is not used
-app.get("/ping", (_req, res) => {
-    console.log("someone ping here");
-    res.send("pong");
-});
+app.get('/ping', (_req, res) => {
+  console.log('someone ping here')
+  res.send('pong')
+})
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
